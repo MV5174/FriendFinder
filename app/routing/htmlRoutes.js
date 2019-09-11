18d8a92330860770773945a41b26aa3788d1d6c6
Michael Vega <ichigo509@yahoo.com>
2019-09-11 03:33:53 -0400

    added a / route for the home page

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,6 +6,9 @@ module.exports = function (app) {
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 
     //A default, catch-all route that leads to home.html which displays the home page.
     app.get("*", function (req, res) {
