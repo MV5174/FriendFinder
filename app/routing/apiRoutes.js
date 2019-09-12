2b966c641e268aa4e0cf2f75713b0835f501374d
Michael Vega <ichigo509@yahoo.com>
2019-09-11 23:23:07 -0400

    added new friends and fixed matching function

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,31 +9,32 @@ module.exports = function (app) {
     });
 
     //A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-    app.post("/api/friends", function (req,res){
+    app.post("/api/friends", function (req, res) {
         var userInp = req.body
         var bestMatch = {};
         var bestScore = 100;
-          for (i = 0; i < friendsData.length; i ++){
-              var currentFriend = friendsData[i];
-              var currentScore = 0;
-              for (j = 0; j < userInp.scores.length; j ++){
+        for (i = 0; i < friendsData.length; i++) {
+            var currentFriend = friendsData[i];
+            var currentScore = 0;
+            for (j = 0; j < userInp.scores.length; j++) {
                 currentScore += Math.abs(userInp.scores[j] - currentFriend.scores[j])
-              }
-              
-              console.log("current friend: " + currentFriend.name);
-              console.log("current score: " + currentScore);
+            }
 
-              if (currentScore < bestScore){
-                  bestScore = currentScore;
-                  bestMatch.name = currentFriend.name;
-                  bestMatch.pic = currentFriend.pic
-                  
-              }
+            console.log("current friend: " + currentFriend.name);
+            console.log("current score: " + currentScore);
+
+            if (currentScore < bestScore) {
+                bestScore = currentScore;
+                bestMatch.name = currentFriend.name;
+                bestMatch.pic = currentFriend.pic
+
+            }
+
+        }
         console.log("best match: " + bestMatch.name);
         console.log("best score: " + bestScore)
         res.json(bestMatch);
-    }
-})
+    })
 
     //code so you could clear out the table while working with the functionality.
 
